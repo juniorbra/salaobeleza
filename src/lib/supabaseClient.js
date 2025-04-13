d078c880d82fa64e2e8bc1aa4814af5738c2bd0a
juniorbra <hvidigaljr@gmail.com>
2025-04-13 14:37:52 -0300

    v10 Credenciais do Supabase ajustadas

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Essas variáveis devem ser substituídas pelas suas credenciais do Supabase
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Credenciais do Supabase
+// Definindo valores fixos para garantir que funcionem em produção
+const supabaseUrl = "https://ipjxqbnuaioaircxsuix.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwanhxYm51YWlvYWlyY3hzdWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NTk3ODAsImV4cCI6MjA2MDEzNTc4MH0.7K7WXD_eOV4JULX20vpgVkNZUm_mHaA_gjfTwOnpU_g";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
